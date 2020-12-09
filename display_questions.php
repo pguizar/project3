3f22f224a9ff4063b032f51e49fe934c441bdaa1
pguizar <70920704+pguizar@users.noreply.github.com>
2020-12-08 21:02:20 -0500

    Update display_questions.php

--- display_questions.php
+++ display_questions.php
@@ -23,6 +23,15 @@
   				<td><?php echo $question['title']; ?></td>
   				<td><?php echo $question['body']; ?></td>
           <td><?php echo $question['skills']; ?></td>
+          <td>
+            <form action="." method="post">
+              <input type="hidden" name="action" value="delete_question">
+              <input type="hidden" name="questionId" value="<?php echo $question['id']; ?>">
+              <input type="hidden" name="userId" value="<?php echo $userId; ?>">
+
+              <input type="submit" value="Delete">
+              <input type="submit" value="Edit">
+          </td>
   			</tr>
   		<?php endforeach; ?>	
   	</table>
