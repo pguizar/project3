8a4f843c0c58a27acdf6c2b0933a247a5b8054fe
pguizar <70920704+pguizar@users.noreply.github.com>
2020-12-14 19:43:35 -0500

    Update pdo.php

--- model/pdo.php
+++ model/pdo.php
@@ -1,1 +1,15 @@
+<?php
+$username = 'bcn3_proj';
+$password = 'w9hLvRFP';
+$hostname = 'sql1.njit.edu';
+$dsn = "mysql:host=$hostname;dbname=$username";
+try {
+    $db = new PDO($dsn, $username, $password);
 
+    
+
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+?>
