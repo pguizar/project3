63cfb9adce7761f123b4c3a6790c765f99d74520
pguizar <70920704+pguizar@users.noreply.github.com>
2020-12-14 20:55:38 -0500

    Update registration.php

--- registration.php
+++ registration.php
@@ -9,7 +9,7 @@
     <nav> 
       <ul>
         <li> <a href = "login.php" > Login </a> </li>
-        <li> <a href = "newQuestionForm.php" > New Question Form </a> </li>
+        
       </ul>
   </nav>
 <?php
