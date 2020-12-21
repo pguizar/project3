453e8e99797796286ced1de6f171e934139ec60c
pguizar <70920704+pguizar@users.noreply.github.com>
2020-12-20 22:04:53 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 require('pdo.php');
+require('account.php');
+require('questions.php');
 require('accounts_db.php');
 require('questions_db.php');
+
+
+session_start();
 
 $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
@@ -12,7 +17,11 @@ if ($action == NULL) {
 }
 switch ($action) {
     case 'show_login': {
+        if($_SESSION['userId']) {
+            header('Location: .?action=display_questions');
+        } else {
         include('login.php');
+    }
         break;
     }
     case 'validate_login': {
@@ -20,16 +29,16 @@ switch ($action) {
         $password = filter_input(INPUT_POST, 'password');
         if ($email == NULL || $password == NULL){
             echo 'Email and password not included';
-        } 
-        else {
-            $userId = validate_login($email, $password);
+        } else {
+            $user = AccountsDB::validate_login($email, $password);
+            $userId = $user->getId();
             if ($userId == false) {
-                //echo "Invalid login";
                 header('Location: index.php?action=display_registration.php');
             } 
             else {
                 //echo "Valid login";
-                header("Location: .?action=display_questions&userId=$userId");
+                $_SESSION['userId'] = $userId;
+                header("Location: .?action=display_questions");
             }   
         }
         break;
@@ -42,15 +51,17 @@ switch ($action) {
 
     }
     case 'display_questions': {
-        $userId = filter_input(INPUT_GET, 'userId');
+        $userId = $_SESSION['userId'];
         $listType = filter_input(INPUT_GET, 'listType');
         if ($userId == NULL || $userId < 0) {
             header('Location: .?action=display_login');
         } 
         else 
         {
-            $questions = get_users_questions($userId);
-            $questions = ($listType === 'all') ? get_all_questions() : get_users_questions($userId);
+            $questions = QuestionsDB::get_users_questions($userId);
+            
+            //??? v
+            $questions = ($listType === 'all') ? QuestionsDB::get_all_questions() : QuestionsDB::get_users_questions($userId);
             include('display_questions.php');
         }
         break;
@@ -80,8 +91,8 @@ switch ($action) {
         }
         else
         {
-            create_question($title, $body, $skills, $userId);
-            header("Location: .?action=display_questions&userId=$userId");
+            QuestionsDB::create_question($title, $body, $skills, $userId);
+            header("Location: .?action=display_questions");
         }
         break;
 
@@ -93,9 +104,22 @@ switch ($action) {
             echo 'All Fields are required';
         }
         else {
-            delete_question($questionId);
-            header("Location: .?action=display_questions&userId=$userId");
+            QuestionsDB::delete_question($questionId);
+            header("Location: .?action=display_questions");
         }
+        break;
+    }
+    case 'logout' : {
+        session_destroy();
+        $_SESSION = array();
+        $name = session_name();
+        $expire = strtotime('-1 year');
+
+        $params = session_get_cookie_params();
+
+        setcookie($name, '', $expire, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+
+        header('Location: index.php');
         break;
     }
     } 
